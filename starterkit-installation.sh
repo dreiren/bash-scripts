1dd92c49a65dd018d9263d75ec36c599a4c66244
ad <adrian.ravis@gmail.com>
2013-10-19 10:31:31 +0800

    Create starterkit-installation.sh

---
+++ starterkit-installation.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+#
+#This bash script is to install applications
+
+
+#Update ubuntu and download all the application needed for web development
+echo -e '\033[33m----------Starting ubuntu update and programs installation----------\033[33m'
+echo -e "\033[0m"
+sudo apt-get update
+
+sudo apt-get install mysql-server-5.5 php5 php5-mysql php5-gd apache2 phpmyadmin geany gnome-shell drush -y
+
+sudo a2enmod rewrite
+
+sudo a2enmod vhost_alias
+
+sudo service apache2 restart
+
+

3f46b6f9146e7eba5a91bde93af8bc9e5f415258
Japo Domingo <japodomingo@gmail.com>
2013-11-05 20:58:31 +1100

    Updated and revised drupal-and-fake-domain-installation.sh

--- drupal-and-fakedomain-installation.sh
+++ drupal-and-fakedomain-installation.sh
@@ -1,147 +1,130 @@
-#!bin/bash
-#Drupal Installation and Fake Domain Creation
+#!/bin/bash
+# Drupal Installation and Fake Domain Creation
+#
+# Created by Adrian Ravis 2013-10-22
+# https://github.com/dreiren
+#
+# Edited by Japo Domingo 2013-11-05
+# https://github.com/japo32
+
 echo ''
 echo -e '\033[33m#---------This will install Drupal in Home Directory---------#\033[33m#'
-sleep 2
 
 read -p 'Press [Enter] Key To Start'
+    echo -e "\033[0m"
 sleep 2
 
-echo 'This may take a while...Grab a gloves and punch some of your friends! XD'
+DEF_APACHE_SITES=/etc/apache2/sites-available
+
+LOOP=true
+while $LOOP; do
+  read -p "Enter your Apache folder(default=$DEF_APACHE_SITES): " APACHE_SITES
+  if [[ $APACHE_SITES == "" ]]; then
+    APACHE_SITES=$DEF_APACHE_SITES
+  fi
+  if [[ -d $APACHE_SITES ]]; then
+    LOOP=false
+  else
+    echo -e "\033[33m$APACHE_SITES is not a valid directory\033[33m"
+    echo -e "\033[0m"
+  fi
+done
+
+read -p "Enter your VHost filename( created in $APACHE_SITES ): " vhost
+
+DEF_DIRECTORY=/home/$USER
+
+LOOP=true
+while $LOOP; do
+  read -p "Where will your install folder be stored.(default=$DEF_DIRECTORY): " DIRECTORY
+  if [[ $DIRECTORY == "" ]]; then
+    DIRECTORY=$DEF_DIRECTORY
+  fi
+  if [[ -d $DIRECTORY ]]; then
+    LOOP=false
+  else
+    echo -e "\033[33m$DIRECTORY is not a valid directory\033[33m"
+    echo -e "\033[0m"
+  fi
+done
+
+read -p "Enter the name of your install folder( created in $DIRECTORY ): " docroot
+docroot=$DIRECTORY/$docroot
+
+read -p "Enter your your Server Name( ex. dev.mysite.com ): " servername
+read -p "Type host ( ex. 127.0.0.1 ): " host
+read -p "Enter Drupal version: ( ex. 7.23 ): " dversion
+
+
+echo 'This may take a while...Grab some gloves and punch some of your friends! XD'
 sleep 4
 
-#Install drupal in home directory
-echo '----------Installing Drupal----------'
-echo '----------Supply Root Password---------'
-echo -e "\33m[0m"
-sudo wget http://ftp.drupal.org/files/projects/drupal-7.23.tar.gz
+#Download Drupal
+echo "Downloading Drupal"
+sudo wget http://ftp.drupal.org/files/projects/drupal-$dversion.tar.gz
 
 
-#Ceate Fake Domain
-echo -e '\033[33m----------Create Fake Domain----------\033[33m'
+#Create VHost file and setup Virtual Host
+echo -e '\033[33m----------Virtual Hosts Setup ----------\033[33m'
 echo -e "\033[0m"
-sleep 2
+echo "Creating $APACHE_SITES/$vhost"
+sudo touch $APACHE_SITES/$vhost
 
-#This will accept the inputed fake domain file name from the user
-echo -e '\033[33m----------Please Enter your Fake Domain name----------\033[33m'
-echo -e "\033[0m"
-echo "Enter your Fake Domain Name( ex. myFakedomainName )" 
-read dname
-sudo touch /etc/apache2/sites-available/$dname
-
-#Changing the Directory
-echo -e '\033[33m----------Changing Directory----------\033[33m'
-echo -e "\033[0m"
-sleep 2
-cd /etc/apache2/sites-available/
-
-
-#This text will be inputed in the /etc/apache2/sites-available/auinp text file
-echo -e '\033[33m----------Adding Text to the Text File----------\033[33m'
-echo -e "\033[0m"
-sleep 2
-
-#This will accept the inputed fake domain file name from the user
-echo "Enter your your Server Name(Ex. dev.fakedomain.com ): "
-read servername
-echo "Enter your your Document Root(Ex. /home/user/foldername ): "
-read docroot
-
-echo "<Virtualhost :*80>
-	ServerName $servername
-	DocumentRoot $docroot
-	</Virtualhost>" | sudo tee $dname #in Exchange of > sudo fake domain name
-echo -e '\033[33m----------Adding Document Root folder----------\033[33m'
-echo -e "\033[0m"
-sleep 2
+echo "<Virtualhost *:80>
+  ServerName $servername
+  DocumentRoot $docroot
+</Virtualhost>" | sudo tee $APACHE_SITES/$vhost
+echo "Creating $docroot"
 sudo mkdir $docroot
-
-
-#Adding the created faked domain to hosts
-echo -e '\033[33m----------Add the Fake domain to hosts----------\033[33m'
-echo -e "\033[0m"
 sleep 2
 
-#Changing directory to  /etc/
-echo -e '\033[33m----------Change Directory to /etc/----------\033[33m'
-echo -e "\033[0m"
+#Append the another virtual host to the hosts text file
+echo "Adding $host $servername to /etc/hosts"
+echo "$host $servername" | sudo tee -a /etc/hosts
 sleep 2
-cd /etc/
 
-#Append the another fake domain to the hosts text file
-echo -e '\033[33m----------Editing /etc/hosts----------\033[33m'
-echo -e "\033[0m"
+#enable the virtual host
+echo "Enabling VHost"
+sudo a2ensite $vhost
 sleep 2
-echo "Type host (Ex. 127.0.1.1): "
-read host
-echo "$host $servername" | sudo tee -a hosts #in Exchange of >> sudo hosts to append this text
-
-
-#enable the fake domain
-echo -e '\033[33m----------Enabling the Fake domain----------\033[33m'
-echo -e '\033[0m'
-sleep 2
-sudo a2ensite $dname
-
-#Reloading apache2
-echo -e '\033[33m----------Reloading apache2----------\033[33m'
-echo -e "\033[0m"
-sleep 2
-sudo service apache2 reload
-
-#Changing directory to home
-echo -e '\033[33m----------Change Directory to /etc/----------\033[33m'
-echo -e "\033[0m"
-sleep 2
-cd
-
 
 #Extract drupal
 echo -e '\033[33m----------Extracting drupal-7.23.tar.gz----------\033[33m'
 echo -e "\033[0m"
 sleep 2
-sudo tar -zxvf ~/drupal-7.23.tar.gz
+sudo tar -zxvf drupal-$dversion.tar.gz
 
 #Move drupal files to /var/www/auinp
 echo -e '\033[33m----------Moving drupal files to fake domain folder----------\033[33m'
 echo -e "\033[0m"
 sleep 2
-sudo cp -R  ~/drupal-7.23/  $docroot
-
-#Creating folder "files" in /var/www/$docroot 
-echo -e '\033[33m----------Creating folder "files" in $docroot/sites----------\033[33m'
-echo -e "\033[0m"
-sleep 2
-sudo mkdir $docroot/sites/default/files
+# added explicit inclusion for the hidden files
+sudo cp -R  drupal-$dversion/* drupal-$dversion/.htaccess drupal-$dversion/.gitignore $docroot
 
 #Creating a settings.php from default.settings.php
-echo -e '\033[33m----------Creating a settings.php ----------\033[33m'
+echo -e '\033[33m----------Settings.php and files folder ----------\033[33m'
 echo -e "\033[0m"
 sleep 2
 sudo cp $docroot/sites/default/default.settings.php $docroot/sites/default/settings.php
-
-
-#Set Permission as a+rwx to created files folder and settings.php
-echo -e '\033[33m----------Set Permission to created files folder and settings.php ----------\033[33m'
-echo -e "\033[0m"
-sleep 2
-sudo chmod uga+rwx $docroot/sites/default/settings.php && sudo chmod uga+rwx $docroot/sites/default/files
+sudo mkdir $docroot/sites/default/files
+echo "Created files and settings.php"
+sudo chmod uga+rwx $docroot/sites/default/settings.php
+sudo chmod uga+rwx $docroot/sites/default/files
+echo "Changed write permissions for files and settings.php"
+echo "--> Remember to remove write permissions for settings.php after installation"
 
 
 #Create database for druapal site
-echo -e '\033[33m----------Create database for druapal site----------\033[33m'
+echo -e '\033[33m----------Create database for drupal site----------\033[33m'
 echo -e "\033[0m"
 
-echo "Enter database Username: "
-read  uname
-echo "Enter database password: "
-read  -s pass #-s used to hide the password input of the user
-echo "Name of Database: "
-read  dbname
+read -p "Enter database Username: " uname
+read -sp "Enter database password: " pass #-s used to hide the password input of the user
+echo ""
+read -p "Name of Database: " dbname
 #Create database
 mysqladmin -u$uname -p$pass create $dbname
-echo -e '\033[33m----------Creating Database Successful----------\033[33m'
-echo -e "\033[0m"
+echo "Created $dbname"
 
 
 
@@ -153,6 +136,7 @@ sudo service apache2 restart
 
 #The End of the Installation
 echo -e '\033[33m----------The End of the Drupal Installation----------\033[33m'
-echo '----------You can now Install your Drupal in your browser--------'
+echo "----------You can now Install your Drupal in your browser--------"
+echo "----------Please check $servername in your browser--------"
 echo '----------Thank You!--------'
 echo -e "\033[0m"
